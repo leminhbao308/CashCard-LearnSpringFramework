6554c18c92a640eb3916efc2b1b3d125928ea2b6
Lê Minh Bảo <besilent308@gmail.com>
2024-11-11 15:33:53 +0700

    Create Project

---
+++ src/test/java/com/leminhbao/cashcard/CashCardApplicationTests.java
@@ -0,0 +1,14 @@
+package com.leminhbao.cashcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+		throw new UnsupportedOperationException("notImplemented() cannot be performed because the test environment is not set up.");
+	}
+
+}
